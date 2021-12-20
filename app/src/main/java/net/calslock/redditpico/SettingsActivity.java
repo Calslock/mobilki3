6f65e8584e05806000eefaad6126a152b4a64f30
Calslock <admin@calslock.net>
2021-12-20 17:23:04 +0100

    Added changeTheme functionality

--- app/src/main/java/net/calslock/redditpico/SettingsActivity.java
+++ app/src/main/java/net/calslock/redditpico/SettingsActivity.java
@@ -1,12 +1,20 @@
 package net.calslock.redditpico;
 
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM;
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO;
+import static androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_YES;
+
 import android.os.Bundle;
 
+import android.util.Log;
 import android.view.MenuItem;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.NavUtils;
+import androidx.preference.ListPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -39,6 +47,30 @@ public class SettingsActivity extends AppCompatActivity {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            ListPreference themePreference = findPreference("theme");
+            assert themePreference != null;
+            updateTheme(themePreference.getValue());
+            themePreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                updateTheme(newValue.toString());
+                return true;
+            });
+        }
+
+        public void updateTheme(String newValue){
+            int targetTheme;
+            switch (newValue) {
+                default:
+                case "sysdef":
+                    targetTheme = MODE_NIGHT_FOLLOW_SYSTEM;
+                    break;
+                case "dark":
+                    targetTheme = MODE_NIGHT_YES;
+                    break;
+                case "light":
+                    targetTheme = MODE_NIGHT_NO;
+                    break;
+            }
+            AppCompatDelegate.setDefaultNightMode(targetTheme);
         }
     }
 }
